aad88779262d658952bb36c19e71d29755051a29
Eric Vondunn <ericvondunn40@gmail.com>
2020-12-13 14:50:47 -0500

    git ignore

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.use(express.static("public"));
 //remove?
 const databaseUrl = "workouts_db";
 const collections = ["workouts"];
+// mongodb+srv://evondunn:Milk40!!17@cluster0.ywhev.mongodb.net/workouts_db?retryWrites=true&w=majority
 
+//mongodb://localhost/workouts_db
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts_db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
